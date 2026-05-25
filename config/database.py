6b91ea7e272c4cacfed3d8f03678b0ae614bd47f
danilo <danilo.mandusic1@gmail.com>
2026-05-25 16:43:23 +0200

    updated .gitignore

--- config/database.py
+++ config/database.py
@@ -1,8 +1,8 @@
 
 DATABASE_CONFIG = {
-    "host": "185.29.103.47",
-    "port": 3306,
-    "database": "zadatak",
-    "user": "zadatak",
-    "password": "vahjoohieH0kee3c",
+    "host": "",
+    "port": None,
+    "database": "",
+    "user": "",
+    "password": "",
 }
