71eab8f3ca0b844020fb7db37f92afbee8433af3
danilo <danilo.mandusic1@gmail.com>
2026-05-25 16:39:50 +0200

    cleaning

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 DATABASE_CONFIG = {
-    "host": "185.29.103.47",
-    "port": 3306,
-    "database": "zadatak",
-    "user": "zadatak",
-    "password": "vahjoohieH0kee3c",
+    "host": "",
+    "port": None,
+    "database": "",
+    "user": "",
+    "password": "",
 }
